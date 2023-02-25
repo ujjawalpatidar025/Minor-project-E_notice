63e43b18112a0e05bcb41efde3b1ebd25df9185c
vibha mishra <vkmmishra871@gmail.com>
2023-02-25 23:14:15 +0530

    setting the routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,33 @@
+import { Route, Routes} from "react-router-dom";
+import Contact from "./Pages/contact";
+import Home from "./Pages/Home";
+import Landing from "./Pages/Landing";
+import Layout from "./Pages/Layout";
+import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from "react";
+
 
 
 function App() {
+  let location=useLocation();
+  const [layout, setLayout]=useState(false);
+  
+  useEffect(() => {
+      if(location.pathname!=='/') setLayout(true);
+  }, [location])
+  
   return (
     <div className="App">
-      Hello Vibha
+      {
+          layout&&<Layout/>
+      }
+      <Routes>
+        <Route path="/" >
+          <Route index element={<Landing />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
